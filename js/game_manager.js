ecaa96a3f7faf78cea30ab47115490ffc06e1092
Doggy-Car <s14014.hrgps.edu.hk@gmail.com>
2024-10-05 11:16:17 +0800

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -167,8 +167,8 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 64) self.won = true;
-        } else if (merged.value === 2048) self.won = true;
+          if (merged.value === 64) {self.won = true;
+        } else if (merged.value === 2048) {self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
